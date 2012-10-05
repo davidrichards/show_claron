6610deb85a74eae8841ee353ce472a3c2df03c63
David Richards <davidlamontrichards@gmail.com>
2012-10-04 19:20:10 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowClaron::Application.config.secret_token = 'e3b80a358fc1bb7a8b7b500c6d53c7258ddcecbe2dd327a97349ff3bda5c3265ca7ff7cac922bb231cef4806aca17d7620139addfeff16252091ff3d8972c4f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShowClaron::Application.config.session_store :cookie_store, key: '_show_claron_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShowClaron::Application.config.session_store :active_record_store
